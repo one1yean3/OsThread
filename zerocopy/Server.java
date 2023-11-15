acec96c7ad1061eacc38ffa6e3a8e35600db2dea
640710566 Wannueng Yoosomboon <87028011+one1yean3@users.noreply.github.com>
2023-11-15 12:44:17 +0700

    Add files via upload

--- zerocopy/Server.java
+++ zerocopy/Server.java
@@ -62,70 +62,48 @@ public class Server {
                         case "1":
                             sendFileList();
                             break;
-                        case "2":
-                            String fileName = in.readLine();
-                            File file = new File(serverFilePath + fileName);
-                            if (file.exists()) {
-                                out.println(file.length());
-                            } else {
-                                out.println(-1);
-                            }
-                            break;
-                        case "3":
-                            String fileName2 = in.readLine();
-                            File file2 = new File(serverFilePath + fileName2);
-
-                            if (file2.exists()) {
-                                FileChannel fc = new FileInputStream(serverFilePath + fileName2).getChannel();
-                                System.out.println(file2.length());
-                                long fileS = file2.length();
-                                System.out.println(fc.size());
+                        case "ZEROCOPY":
+                            String fileName1 = in.readLine();
+                            File fileZ = new File(serverFilePath + fileName1);
+                            if (fileZ.exists()) {
+                               
+                                FileChannel fc = new FileInputStream(serverFilePath + fileName1).getChannel();
                                 out.println(fc.size());
-
-                                long totalBytesTransferred = 0;
-                                while (totalBytesTransferred < fileS) {
-                                    long bytesTransferred = fc.transferTo(totalBytesTransferred,
-                                            fc.size() - totalBytesTransferred, clientSocketChannel);
-                                    System.out.println(bytesTransferred);
-                                    totalBytesTransferred += bytesTransferred;
+                                long transferred = 0;
+                                while (transferred < fc.size()) {
+                                    long count = fc.transferTo(transferred,
+                                            fc.size() - transferred, clientSocketChannel);
+                                    transferred += count;
                                 }
                             }
-                            break;
-                        case "4":
-                            String fileName3 = in.readLine();
-                           
-                            FileInputStream file3 = new FileInputStream(serverFilePath + fileName3);
-                            
-
-                            OutputStream os1 = clientSocket.getOutputStream();
-                            byte[] buffers = new byte[64 * 1024];
-                            while ( file3.read(buffers) != -1) {
-                                os1.write(buffers);
+                            else{
+                                clientSocket.close();
                             }
-                            os1.close();
-                            file3.close();
+           
                             break;
-                        case "DOWNLOAD":
-
-                            String fileN = in.readLine();
-                            long start = Long.parseLong(in.readLine());
-                            long end = Long.parseLong(in.readLine());
-
-                            RandomAccessFile raf = new RandomAccessFile(new File(serverFilePath + fileN), "r");
-
-                            byte[] buffer = new byte[64 * 1024];
-                            int bytesRead;
-
-                            raf.seek(start);
-
-                            OutputStream os = clientSocket.getOutputStream();
-
-                            while (start < end && (bytesRead = raf.read(buffer)) != -1) {
-                                os.write(buffer);
-                                start += bytesRead;
+                        case "TRADITIONAL":
+                        try{
+                            String fileName2 = in.readLine();
+            
+                            File fileT = new File(serverFilePath + fileName2);
+  
+                            if(fileT.exists()){
+                                
+                                FileInputStream fis = new FileInputStream(serverFilePath + fileName2);
+                                OutputStream os = clientSocket.getOutputStream();
+                                byte[] buffers = new byte[64 * 1024];
+                                while (( fis.read(buffers)) != -1) {
+                                    os.write(buffers);
+                                }
+                                os.close();
+                                fis.close();
                             }
-                            raf.close();
-                            os.close();
+               
+                        }
+                        catch(Exception e){
+                          
+                            e.printStackTrace();
+                        }
                             break;
                         default:
                             System.out.println("Wrong input from user");
